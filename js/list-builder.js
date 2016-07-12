b6ee06f1e337370bf8f7c693f50460dc64e45917
Winston Hearn <winston.hearn@voxmedia.com>
2016-07-12 15:48:22 -0500

    updating list building a bit more

--- js/list-builder.js
+++ js/list-builder.js
@@ -11,41 +11,49 @@ new Clipboard('.c-guidelines__copy');
     return false;
   }
 
+  var formatters = {
+    github: "\n- [ ] ",
+    markdown: "\n - ",
+    plaintext: "\n"
+  };
+
   var checkboxes = $$('input[type="checkbox"]');
   var textarea = container.querySelector('.preview-box');
   var outputType = document.getElementById('output').value;
   var active = [];
 
+  function titleCase(str) {
+    return str.charAt(0).toUpperCase() + str.substr(1);
+  }
+
   function formatPreview(start) {
-    start = start ? start : "";
-    var ret = active.reduce(function(prev, curr, idx){
-      var str = curr[0];
+    var activeSection = "";
+
+    return active.reduce(function(prev, curr, idx){
+      var currentSection = curr[1].split("-")[0];
+      var str = "";
+      if (activeSection !== currentSection) {
+        activeSection = currentSection;
+        str = "## " + titleCase(currentSection);
+      }
+
+      str = str + start + curr[0];
+
       if (outputType !== "plaintext") {
         str = str + " (More Info)[" + window.location.href + "#" + curr[1] + "]";
       }
-      return idx === 0 ? start + str : prev + start + str;
+
+      return idx > 0 ?  prev + str : str;
     }, active[0]);
-    return ret;
   }
 
   function outputPreview() {
     if (!active.length) { return false;}
-    var preview;
-    switch (outputType) {
-      case "github":
-        preview = formatPreview("\n- [ ] ");
-        break;
-      case "markdown":
-        preview = formatPreview("\n - ");
-        break;
-      default:
-        preview = active.join("\n");
-    }
-    textarea.value = preview;
+    textarea.value = formatPreview(formatters[outputType]);
   }
 
   function rebuildActive() {
-    var newActive = []
+    var newActive = [];
     checkboxes.forEach(function(c) {
       if (c.checked) {
         var deets = [c.parentNode.querySelector('p').innerText, c.parentNode.id]
